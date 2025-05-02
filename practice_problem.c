2d252d2c862fd9f163df9e93304a58462de2cb99
bhaveshkhertala <bkhertala@gmail.com>
2025-05-02 15:22:48 +0530

    practice_problem.c

--- practice_problem.c
+++ practice_problem.c
@@ -1,22 +1,11 @@
 #include<stdio.h>
 int main(){
-//  int a,b;
-//  printf("enter the value a");
-//  scanf("%d",&a);
-//  printf("enter the value b");
-//  scanf("%d",&b);
-//  printf("the sum is = %d",a+b);
-// int a,b;
-// printf("enter the value a");
-// scanf("%d",&a);
-// printf("enter the value b");
-// scanf("%d",&b);
-// printf("the sum is = %d",a-b);
+//take input from user and add two values
 int a,b;
 printf("enter the value a");
 scanf("%d",&a);
 printf("enter the value b");
 scanf("%d",&b);
-printf("the sum is = %d",a*b);
+printf("the sum is = %d",a+b);
     return 0;
 }
