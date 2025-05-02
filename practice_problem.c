e92508447f7015d24fe70ea6c0cde92ca2f8b92d
bhaveshkhertala <bkhertala@gmail.com>
2025-05-02 15:07:38 +0530

    Add files via upload

---
+++ practice_problem.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+//  int a,b;
+//  printf("enter the value a");
+//  scanf("%d",&a);
+//  printf("enter the value b");
+//  scanf("%d",&b);
+//  printf("the sum is = %d",a+b);
+// int a,b;
+// printf("enter the value a");
+// scanf("%d",&a);
+// printf("enter the value b");
+// scanf("%d",&b);
+// printf("the sum is = %d",a-b);
+int a,b;
+printf("enter the value a");
+scanf("%d",&a);
+printf("enter the value b");
+scanf("%d",&b);
+printf("the sum is = %d",a*b);
+    return 0;
+}
